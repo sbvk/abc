31bf34b1130175831c93fe7c4bdcdfcc7ccc0732
sb <riyabendigeri@gmail.com>
2020-10-06 12:39:48 +0530

    6 oct

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://sbvk:atlas123@abc.fxne0.mongodb.net/CustomerDB',{useNewUrlParser:true},(err)=>{
+mongoose.connect('mongodb+srv://sbvk:atlas123@abc.fxne0.mongodb.net/CustomerDB',{useNewUrlParser:true, useUnifiedTopology: true},(err)=>{
     if(!err) {console.log('conn succeeded')}
     else{ console.log('Error in conn:'+ err)}
 });
