0697d0cc443b2156a2fdcf257d6e831abd7f8647
sb <riyabendigeri@gmail.com>
2020-10-06 12:20:26 +0530

    6 oct

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CustomerDB',{useNewUrlParser:true},(err)=>{
+mongoose.connect('mongodb+srv://sbvk:atlas123@abc.fxne0.mongodb.net/CustomerDB',{useNewUrlParser:true},(err)=>{
     if(!err) {console.log('conn succeeded')}
     else{ console.log('Error in conn:'+ err)}
 });
